1c63b079376957703620ea7064aa20803bf163ca
Rick <dirklo@gmail.com>
2020-12-24 16:28:43 +0100

    commit tree 4b3ad67b1964ff422d2941d5923eee0deac5cb81

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].attributes
+
+
